eec31a3e09f587a46d974bd909b4f814046a4a71
tamfoi <tamfoi@users.noreply.github.com>
2023-02-13 16:18:03 +0900

    publish: プロトタイプ完成につき公開

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ProfileEditorApp",
+  base: "/ProfileEditor",
+  build: {
+    outDir: "docs"
+  },
   plugins: [react({jsxImportSource: '@emotion/react',})],
 })
